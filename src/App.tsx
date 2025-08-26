012ac4a287a34ce6a90516ec791b8955b27f4a00
Adarshk33 <adarsh.sinha33@gmail.com>
2025-08-26 19:37:59 +0530

    worked on import

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, Suspense, lazy } from "react";
 import { useGlobalLoaderContext } from "./helpers/GlobalLoader";
 import API from "./api";
 import { ROUTES } from "./lib/consts";
-import Home from "./pages/home/Home";
-import RegistrationStep1 from "./pages/registration/RegistrationStep1";
-import RegistrationStep2 from "./pages/registration/RegistrationStep2";
-import VerificationOtp from "./pages/verificationOtp/VerificationOtp";
-import Redemption from "./pages/redemption/Redemption";
-import CashBack from "./pages/cashBack/CashBack";
-import KYC from "./pages/kyc/kyc";
-import RewardScreen from "./pages/congrats/RewardScreen";
 
-import OtpRedeemVerification from "./pages/verificationRedeemOtp/VerificationRedeemOtp";
-import EnvelopeAnimation from "./pages/EnvelopeAnimation/EnvelopeAnimation";
-
-
-
+// Lazy load pages
+const Home = lazy(() => import("./pages/home/Home"));
+const RegistrationStep1 = lazy(() => import("./pages/registration/RegistrationStep1"));
+const RegistrationStep2 = lazy(() => import("./pages/registration/RegistrationStep2"));
+const VerificationOtp = lazy(() => import("./pages/verificationOtp/VerificationOtp"));
+const Redemption = lazy(() => import("./pages/redemption/Redemption"));
+const CashBack = lazy(() => import("./pages/cashBack/CashBack"));
+const KYC = lazy(() => import("./pages/kyc/kyc"));
+const RewardScreen = lazy(() => import("./pages/congrats/RewardScreen"));
+const OtpRedeemVerification = lazy(() => import("./pages/verificationRedeemOtp/VerificationRedeemOtp"));
+const EnvelopeAnimation = lazy(() => import("./pages/EnvelopeAnimation/EnvelopeAnimation"));
 
 function App() {
   const { showLoader, hideLoader } = useGlobalLoaderContext();
@@ -25,28 +23,16 @@ function App() {
   useEffect(() => {
     API.initialize(showLoader, hideLoader);
 
-    // if (!isLoggedIn) {
-    //   API.createUser().then((response) => {
-    //     store.dispatch(setUserKey(response));
-    //     if (!response.isLoggedIn && isLoggedIn) {
-    //       logoutUser();
-    //       navigate(ROUTES.REGISTER);
-    //       toast.info("Your session has been expired");
-    //     }
-    //   });
-    // }
-
     window.addEventListener("online", () => {
       API.setIsOnline(true);
     });
     window.addEventListener("offline", () => {
       API.setIsOnline(false);
     });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [showLoader, hideLoader]);
 
   return (
-    <>
+    <Suspense fallback={<div className="loader">Loading...</div>}>
       <Routes>
         <Route path={ROUTES.HOME} element={<Home />} />
         <Route path={ROUTES.RegistrationStep1} element={<RegistrationStep1 />} />
@@ -55,19 +41,11 @@ function App() {
         <Route path={ROUTES.Redemption} element={<Redemption />} />
         <Route path={ROUTES.CashBack} element={<CashBack />} />
         <Route path={ROUTES.KYC} element={<KYC />} />
-        <Route path={ROUTES.REWARDSCREEN} element={<RewardScreen/>} />
-        <Route path={ROUTES.VERIFICATIONREDEEMOTP} element={<OtpRedeemVerification/>} />
-        <Route path="/EnvelopeAnimation" element={<EnvelopeAnimation/>} />
-
-      
-
-
-
-
-
-
+        <Route path={ROUTES.REWARDSCREEN} element={<RewardScreen />} />
+        <Route path={ROUTES.VERIFICATIONREDEEMOTP} element={<OtpRedeemVerification />} />
+        <Route path="/EnvelopeAnimation" element={<EnvelopeAnimation />} />
       </Routes>
-    </>
+    </Suspense>
   );
 }
 
